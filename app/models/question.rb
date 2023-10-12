c4396d6946cdda1cef8394465260e0c1ea3b9477
fuuuuumin <tmfha3@gmail.com>
2023-10-12 20:08:45 +0900

    バリデーションの設定

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ApplicationRecord
 
   belongs_to :user
 
+  validates :question_title, presence; true
+  validates :question_content, presence; true
+  validates :language_id, presence; true
+
 end
