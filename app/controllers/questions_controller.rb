b73b56a229b7c8421feda466d7cbfd7cbe2f40da
fuuuuumin <tmfha3@gmail.com>
2023-10-13 19:47:39 +0900

    質問を投稿できる

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,11 +5,22 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    
+    @question = Question.new
   end
 
   def create
-    
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:question_title, :question_content, :language_id).merge(user_id: current_user.id)
   end
 
 end
