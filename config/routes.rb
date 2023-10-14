a28ffea7c3d5486855c5f7e53fe68aede80eaa94
fuuuuumin <tmfha3@gmail.com>
2023-10-14 12:39:51 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'homes#index'
-  resources :questions, only: [:index, :new, :create, :show]
+  resources :questions, only: [:index, :new, :create, :show] do
+    resources :answers, only: :create
+  end
 
 end
