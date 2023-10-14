5be732cd3637c1565601f2d3b5d08e751d984a4d
fuuuuumin <tmfha3@gmail.com>
2023-10-14 18:43:38 +0900

    コントローラーの作成とルーディング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :questions, only: [:index, :new, :create, :show] do
     resources :answers, only: :create
   end
+  resources :profile, only: [:index, :new, :create]
 
 end
