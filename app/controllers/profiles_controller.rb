7b122bf82e63cb980577943f9de6383effde7266
fuuuuumin <tmfha3@gmail.com>
2023-10-14 19:02:15 +0900

    ニックネームとプロフィールを保存できる

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,22 @@
 class ProfilesController < ApplicationController
 
   def new
-    
+    @profile = Profile.new
   end
 
   def create
-    
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:nickname, :introduction).merge(user_id: current_user.id)
+  end
+
 end
