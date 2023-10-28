e50893659b29dcc0f6659a51106684d30f7cffec
fuuuuumin <tmfha3@gmail.com>
2023-10-28 12:47:21 +0900

    バリデーション設定

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -5,4 +5,7 @@ class ChatRoom < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :language
 
+  validates :room_name, presence: true
+  validates :language_id, numericality: { other_than: 1 , message: "チャットルームで使用できる言語を選択してください" }
+
 end
